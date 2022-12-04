46cb2027ec31bee135f7f96e24ea987a389c7d28
Robert Schlereth <robert.schlereth@gmail.com>
2022-12-03 18:37:03 -0600

    Add console statements

--- variables.js
+++ variables.js
@@ -6,3 +6,10 @@ var weight = 200
 
 var freezing = true
 var obese = false 
+
+streetAddress = "789 Duck Rd."
+placeOfBirth = "Detroit, MI"
+
+console.log(streetAddress, placeOfBirth)
+console.log(temperature, weight)
+console.log(freezing, obese)
